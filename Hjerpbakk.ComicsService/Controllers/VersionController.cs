1e381bb1292cfd4cfd8a25b05317d4faecd67b14
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-10-23 11:41:58 +0200

    Added Insights and version API

---
+++ Hjerpbakk.ComicsService/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace Hjerpbakk.ComicsService.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+    }
+}
